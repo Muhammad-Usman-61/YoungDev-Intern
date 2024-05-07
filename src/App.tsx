40bdaaf2359e474972ff14a7a29e375eb9d9ae04
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-05-07 21:59:02 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <Router>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Router>
       <NavBar />
     </>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,49 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-  const tasks = ["Task 1", "Task 2", "Task 3"];
+  const tasks = [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Login",
+      url: "/login",
+    },
+  ];
   const myData = [
     {
       name: "Linkedin",
-      url: "https://www.linkedin.com/in/username",
+      url: "https://www.linkedin.com/in/muhammad-usman-794205247/",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/Muhammad-Usman-61",
     },
   ];
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav className="border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="https://flowbite.com" className="flex items-center">
+          <a
+            href="https://muhammad-usman061.vercel.app/"
+            className="flex items-center"
+          >
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-9"
+              src="src\assets\logo.png"
+              className="mr-3 h-12"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+              YoungDev Inters
             </span>
           </a>
-          <div className="flex items-center lg:order-2">
+          <div className="flex items-center gap-4 lg:order-2 ">
             {myData.map((data) => (
               <a
                 href={data.url}
-                className="px-5 py-2 text-white rounded bg-primary-700 hover:bg-gray-700"
+                key={data.name}
+                className="px-5 py-2 text-white rounded bg-gray-900 hover:bg-gray-700 lg:block hidden"
               >
                 {data.name}
               </a>
@@ -33,7 +52,7 @@ const NavBar = () => {
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 ml-1 text-sm rounded-lg lg:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
@@ -45,9 +64,9 @@ const NavBar = () => {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               <svg
@@ -57,9 +76,9 @@ const NavBar = () => {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </button>
@@ -68,16 +87,16 @@ const NavBar = () => {
             className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <ul className="flex font-medium gap-4">
               {tasks.map((task) => (
-                <li>
-                  <a
-                    href="#"
-                    className="px-5 py-2 text-white rounded bg-primary-700 hover:bg-gray-700"
+                <li key={task.name}>
+                  <Link
+                    to="#"
+                    className="px-5 py-2 text-white rounded bg-gray-900 hover:bg-gray-700"
                     aria-current="page"
                   >
-                    {task}
-                  </a>
+                    {task.name}
+                  </Link>
                 </li>
               ))}
             </ul>
