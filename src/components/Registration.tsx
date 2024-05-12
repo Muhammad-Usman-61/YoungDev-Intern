9680f934edae472c39901b511d2495e36dba5c53
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-05-12 23:25:52 +0500

    reg button color updated

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -105,7 +105,7 @@ const Registration = () => {
                     </label>
                   </div>
                 </div>
-                <button className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                <button className="w-full text-white bg-[#92E3A9] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-[#92E3A9] focus:ring-white">
                   Create an account
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
